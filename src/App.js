078249e1f580e90414648bf23ac5e888ddf04fb1
Saklain Mustak <saklainmustak@sakmac.local>
2022-04-04 03:42:52 +0600

    chart added in dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route,  Routes } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Header from "./Components/Header/Header";
 
 import Home from "./Components/Home/Home";
@@ -10,9 +11,11 @@ function App() {
   return (
     <div className="App">
        <Header></Header>
+ 
      <Routes>
        <Route path="/" element={<Home></Home>}></Route>
        <Route path="/reviews" element={<ReviewHeader></ReviewHeader>}></Route>
+       <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
        <Route path="*" element={<NotFound></NotFound>}></Route>
      </Routes>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className=" ">
-        <ul className="flex justify-center bg-orange-300 gap-5 p-6">
-            <Link to="/"  >HOME</Link>
-            <Link to='/reviews'>REVIEWS</Link>
-            <Link to='/dashboard'>DASHBOARD</Link>
-            <Link to='/blogs'>BLOGS</Link>
-            <Link to='/about'>ABOUT</Link>
+        <ul className="flex justify-center bg-orange-300 gap-5 p-6  text-white">
+            <Link className="hover:text-red-500" to="/"  >HOME</Link>
+            <Link className="hover:text-red-500" to='/reviews'>REVIEWS</Link>
+            <Link className="hover:text-red-500" to='/dashboard'>DASHBOARD</Link>
+            <Link className="hover:text-red-500" to='/blogs'>BLOGS</Link>
+            <Link className="hover:text-red-500" to='/about'>ABOUT</Link>
         </ul>
     </div>
        
