2f9f5941d1c39562c88dea54cc8970e6d8eb214f
Saklain Mustak <saklainmustak@sakmac.local>
2022-04-05 01:02:01 +0600

    reviews devided 3 in home and 6 in revies all

--- src/Components/CutomerReviews/CustomerReviews.js
+++ src/Components/CutomerReviews/CustomerReviews.js
@@ -4,13 +4,12 @@ import Reviews from '../CustomHook/Reviews';
 
 const CustomerReviews = () => {
     const [reviews, setReview] = Reviews();
-   
-   
+    const data = reviews.slice(0,3);
     return (
         <div className="text-center">
-            
+        
             {
-                reviews.map(reviews=> <CustomerReviewMap
+                data.map(reviews => <CustomerReviewMap
                 reviews ={reviews}
                 key= {reviews.id}
                 ></CustomerReviewMap>)
